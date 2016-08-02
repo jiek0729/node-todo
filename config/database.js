0a24a1b2a6f7c6d7aa6a73fcd1b07597df4d5954
jie kang <kangx385@umn.edu>
2016-08-02 01:05:08 -0500

    ec2

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
     remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
-    localUrl: 'mongodb://localhost/meanstacktutorials'
+    localUrl: 'mongodb://kangx385:pwd@ec2-52-35-25-216.us-west-2.compute.amazonaws.com:27017/dummyDB'
 };
